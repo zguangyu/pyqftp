78cc2ad761aaf05f0d4314eb9447d173b1e9869e
Guangyu Zhang <zguangyu123@gmail.com>
2014-11-14 02:08:56 +0800

    Support ftp url with password

--- pyqftp/qftp.py
+++ pyqftp/qftp.py
@@ -5,6 +5,7 @@ from ftplib import FTP
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from pyqftp.form_ui import Ui_MainWindow
 from pyqftp.utils import url_check
+from urllib.parse import urlparse
 
 class FtpWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -13,11 +14,28 @@ class FtpWindow(QMainWindow, Ui_MainWindow):
         self.btnOpen.clicked.connect(self.connectFTP)
 
     def connectFTP(self):
+        host = self.txtFtpUrl.text()
+        user = None
+        passwd = None
+        port = 21
+        if url_check(self.txtFtpUrl.text()):
+            res = urlparse(self.txtFtpUrl.text())
+            host = res.hostname
+            user = res.username
+            passwd = res.password
+            port = res.port if res.port else 21
+
+        if user is not None and passwd is None:
+            # TODO: pop up a box to receive password
+            pass
+
         self.lstFileList.clear()
         self.lstFileList.addItem("Loading...")
-        with FTP(self.txtFtpUrl.text()) as ftp:
-            ftp.login(user="ftp", passwd="ftp")
-            flist = list(ftp.nlst())
-            self.lstFileList.clear()
-            for f in flist:
-                self.lstFileList.addItem(f)
+
+        ftp = FTP()
+        ftp.connect(host, port)
+        ftp.login(user, passwd)
+        flist = list(ftp.nlst())
+        self.lstFileList.clear()
+        for f in flist:
+            self.lstFileList.addItem(f)
