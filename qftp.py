1ae8a6c03d659dba0cdc5e791f5e3456f9195859
Guangyu Zhang <zguangyu123@gmail.com>
2014-11-14 12:27:24 +0800

    add company name

--- qftp.py
+++ qftp.py
@@ -3,7 +3,12 @@
 import sys
 from ftplib import FTP
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QCoreApplication
 from pyqftp.qftp import FtpWindow
+
+QCoreApplication.setOrganizationName("Guangyu")
+QCoreApplication.setOrganizationDomain("guangyu.me")
+QCoreApplication.setApplicationName("QFTP")
 
 app = QApplication(sys.argv)
 
